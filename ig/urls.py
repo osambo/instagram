4d71e27e9e98e501c5508223cdb2886f83f2d866
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-13 13:20:34 +0300

    linked urls

--- ig/urls.py
+++ ig/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+from django.contrib import admin
+from django.contrib.auth import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('insta.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
