a1ae00fd107b945b9e0470179f554b8eb9b1fd48
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-13 14:58:07 +0300

    added email sender configs

--- insta/urls.py
+++ insta/urls.py
@@ -3,10 +3,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+# django.contrib.auth.views.login
+# django.contrib.auth.views.LoginView
+# from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     url('^$',views.index,name = 'home'),
     url(r'^signup', views.signup, name='signup'),
+    # url(r'^login/$',auth_views.LoginView.as_view(template_name="useraccounts/login.html"), name="login"),
     url(r'^login', LoginView.as_view(), name='login_url'),
     url(r'^logout/', LogoutView.as_view(next_page='login_url'), name='logout_url'),
     url(r'^newpost/$', views.new_post, name='new_post'),
