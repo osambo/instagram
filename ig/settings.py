e45a8f799cffeb2541ad127ac27005ff8cf9d86d
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-14 13:23:03 +0300

    added auth profile module setting

--- ig/settings.py
+++ ig/settings.py
@@ -113,7 +113,7 @@ EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-
+AUTH_PROFILE_MODULE = 'project.pm.UserProfile'
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
